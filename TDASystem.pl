ec17fdd8e88fade89e072e15f118721027b33261
Ka Zeng <ka.zeng@usach.cl>
2023-06-06 19:33:57 -0400

    constructor

--- TDASystem.pl
+++ TDASystem.pl
@@ -1,1 +1,8 @@
-prueba
+% TDA system - constructor
+% Funcion para crear un sistema
+system(Nombre, Sistema) :-
+    get_time(Timestamp),
+    stamp_date_time(Timestamp, DateTime, local),
+    format_time(atom(FechaCreacion), '%Y-%m-%d', DateTime),
+    Sistema = carpeta(Nombre, FechaCreacion, []),
+    assertz(Sistema).
