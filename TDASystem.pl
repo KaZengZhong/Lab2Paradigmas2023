208c3fe53cdea819662e523123e380ae49780195
Ka Zeng <ka.zeng@usach.cl>
2023-06-06 20:17:42 -0400

    addDrive

--- TDASystem.pl
+++ TDASystem.pl
@@ -6,3 +6,13 @@ system(Nombre, Sistema) :-
     format_time(atom(FechaCreacion), '%Y-%m-%d', DateTime),
     Sistema = carpeta(Nombre, FechaCreacion, []),
     assertz(Sistema).
+
+:- dynamic unidad/4.
+% Añade una unidad al sistema
+systemAddDrive(Sistema, NombreUnidad, Etiqueta, Capacidad, NuevoSistema) :-
+    not(unidad(NombreUnidad, _, _, _)),
+    assertz(unidad(NombreUnidad, Etiqueta, Capacidad, [])),
+    retract(Sistema),
+    Sistema = carpeta(Nombre, FechaCreacion, Contenidos),
+    NuevoSistema = carpeta(Nombre, FechaCreacion, [NombreUnidad|Contenidos]),
+    assertz(NuevoSistema).
