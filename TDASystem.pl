b7fd894c35d7ec06b25433c4dcf5d068980b5699
Ka Zeng <ka.zeng@usach.cl>
2023-06-12 19:45:51 -0400

    systemAddDrive final

--- TDASystem.pl
+++ TDASystem.pl
@@ -1,7 +1,14 @@
 
 % TDA system - constructor
-% Funcion para crear un sistema
-system(Nombre, system(Nombre, Fecha)) :-
+% Predicado constructor de un sistema
+system(Nombre, system(Nombre, Fecha, [])) :-
     get_time(Timestamp),
     stamp_date_time(Timestamp, DateTime, local),
     format_time(string(Fecha), '%Y-%m-%d', DateTime).
+
+% TDA system - addDrive
+% Predicado añadidor de una unidad al sistema
+systemAddDrive(system(Nombre, Fecha, Unidades), Letra, NombreUnidad, Capacidad,
+               system(Nombre, Fecha, [unidad(Letra,NombreUnidad,Capacidad)|Unidades])) :-
+               string_length(Letra, 1),
+               not(member(unidad(Letra, _, _), Unidades)).
