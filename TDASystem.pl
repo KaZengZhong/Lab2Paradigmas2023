7200069adb3adcfdeadb7f0f06384f6e9ccbcfdb
Ka Zeng <ka.zeng@usach.cl>
2023-06-12 19:57:56 -0400

    register

--- TDASystem.pl
+++ TDASystem.pl
@@ -1,14 +1,21 @@
 
 % TDA system - constructor
-% Predicado constructor de un sistema
-system(Nombre, system(Nombre, Fecha, [])) :-
+% Constructor de un sistema
+system(Nombre, system(Nombre, Fecha, [], [])) :-
     get_time(Timestamp),
     stamp_date_time(Timestamp, DateTime, local),
     format_time(string(Fecha), '%Y-%m-%d', DateTime).
 
 % TDA system - addDrive
-% Predicado añadidor de una unidad al sistema
-systemAddDrive(system(Nombre, Fecha, Unidades), Letra, NombreUnidad, Capacidad,
-               system(Nombre, Fecha, [unidad(Letra,NombreUnidad,Capacidad)|Unidades])) :-
-               string_length(Letra, 1),
-               not(member(unidad(Letra, _, _), Unidades)).
+% Añade una unidad al sistema
+systemAddDrive(system(Nombre, Fecha, Usuarios, Unidades), Letra, NombreUnidad, Capacidad,
+               system(Nombre, Fecha, Usuarios, [unidad(Letra,NombreUnidad,Capacidad)|Unidades])) :-
+    string_length(Letra, 1),
+    not(member(unidad(Letra, _, _), Unidades)).
+
+% TDA system - register
+% Añade un usuario al sistema
+systemRegister(system(Nombre, Fecha, Usuarios, Unidades), NombreUsuario,
+               system(Nombre, Fecha, [usuario(NombreUsuario)|Usuarios], Unidades)) :-
+    not(member(usuario(NombreUsuario),Usuarios)).
+
